3a3c667a699e73436c97a056615756828ad186b3
Andreas Grub <andreas.grub@qaware.de>
2019-12-19 16:54:51 +0100

    Start searching the request mapping handler methods

--- openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiGeneratorForSpringAutoConfiguration.java
+++ openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiGeneratorForSpringAutoConfiguration.java
@@ -3,14 +3,15 @@ package de.qaware.openapigeneratorforspring.common;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
 public class OpenApiGeneratorForSpringAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public OpenApiResource openApiResource() {
-        return new OpenApiResource();
+    public OpenApiResource openApiResource(RequestMappingHandlerMapping requestMappingHandlerMapping) {
+        return new OpenApiResource(requestMappingHandlerMapping);
     }
 
 }
--- openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiResource.java
+++ openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiResource.java
@@ -3,16 +3,79 @@ package de.qaware.openapigeneratorforspring.common;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class OpenApiResource {
 
+    private final RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    @Autowired
+    public OpenApiResource(RequestMappingHandlerMapping requestMappingHandlerMapping) {
+        this.requestMappingHandlerMapping = requestMappingHandlerMapping;
+    }
+
     @GetMapping(value = "/v3/api-docs", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getOpenApiAsJson() throws JsonProcessingException {
-        return Json.mapper().writeValueAsString(new OpenAPI());
+
+        Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
+
+        Paths paths = new Paths();
+        map.forEach((info, method) -> {
+            PathItem pathItem = new PathItem();
+            Set<RequestMethod> requestMethods = info.getMethodsCondition().getMethods();
+            requestMethods.forEach(requestMethod -> {
+                Operation operation = new Operation().description(method.getShortLogMessage());
+                setOperationOnPathItem(requestMethod, pathItem, operation);
+            });
+            paths.addPathItem(method.getMethod().getName(), pathItem);
+        });
+
+        OpenAPI openApi = new OpenAPI();
+        openApi.setPaths(paths);
+        return Json.mapper().writeValueAsString(openApi);
+    }
+
+    private static void setOperationOnPathItem(RequestMethod requestMethod, PathItem pathItem, Operation operation) {
+        switch (requestMethod) {
+            case GET:
+                pathItem.setGet(operation);
+                break;
+            case HEAD:
+                pathItem.setHead(operation);
+                break;
+            case POST:
+                pathItem.setPost(operation);
+                break;
+            case PUT:
+                pathItem.setPut(operation);
+                break;
+            case PATCH:
+                pathItem.setPatch(operation);
+                break;
+            case DELETE:
+                pathItem.setDelete(operation);
+                break;
+            case OPTIONS:
+                pathItem.setOptions(operation);
+                break;
+            case TRACE:
+                pathItem.setTrace(operation);
+                break;
+        }
     }
 
 }
