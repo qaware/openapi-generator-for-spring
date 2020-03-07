a5284d7840e0c31c8185bfe0f6d279aa30452590
Andreas Grub <andreas.grub@qaware.de>
2020-03-07 23:03:33 +0100

    Do not return schema if "empty" annotation given

--- openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/schema/SchemaAnnotationMapper.java
+++ openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/schema/SchemaAnnotationMapper.java
@@ -2,10 +2,18 @@ package de.qaware.openapigeneratorforspring.common.schema;
 
 import io.swagger.v3.oas.models.media.Schema;
 
+import javax.annotation.Nullable;
+
 public interface SchemaAnnotationMapper {
+
+
+    @Nullable
     default Schema<Object> buildFromAnnotation(io.swagger.v3.oas.annotations.media.Schema schemaAnnotation, NestedSchemaConsumer nestedSchemaConsumer) {
         Schema<Object> schema = new Schema<>();
         applyFromAnnotation(schema, schemaAnnotation, nestedSchemaConsumer);
+        if (new Schema<>().equals(schema)) {
+            return null;
+        }
         return schema;
     }
 
