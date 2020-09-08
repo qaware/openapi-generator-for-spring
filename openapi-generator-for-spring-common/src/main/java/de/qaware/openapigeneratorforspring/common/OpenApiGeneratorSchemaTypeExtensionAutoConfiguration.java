ae470a4d645bbe254aa8052f242d7b4a4a6e1422
Andreas Grub <andreas.grub@qaware.de>
2020-09-08 16:42:30 +0200

    Move DefaultSpringResponseEntityTypeResolver from test to library

--- openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiGeneratorSchemaTypeExtensionAutoConfiguration.java
+++ openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/OpenApiGeneratorSchemaTypeExtensionAutoConfiguration.java
@@ -3,6 +3,7 @@ package de.qaware.openapigeneratorforspring.common;
 import de.qaware.openapigeneratorforspring.common.schema.resolver.type.extension.java8.DefaultJava8TimeTypeResolver;
 import de.qaware.openapigeneratorforspring.common.schema.resolver.type.extension.java8.Java8TimeTypeResolver;
 import de.qaware.openapigeneratorforspring.common.schema.resolver.type.extension.java8.Java8TimeTypeResolverConfigurationProperties;
+import de.qaware.openapigeneratorforspring.common.schema.resolver.type.extension.spring.DefaultSpringResponseEntityTypeResolver;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +14,11 @@ public class OpenApiGeneratorSchemaTypeExtensionAutoConfiguration {
     @ConditionalOnMissingBean
     public Java8TimeTypeResolver defaultJava8TimeTypeResolver(Java8TimeTypeResolverConfigurationProperties properties) {
         return new DefaultJava8TimeTypeResolver(properties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public DefaultSpringResponseEntityTypeResolver defaultDefaultSpringResponseEntityTypeResolver() {
+        return new DefaultSpringResponseEntityTypeResolver();
     }
 }
--- openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/schema/resolver/type/extension/spring/DefaultSpringResponseEntityTypeResolver.java
+++ openapi-generator-for-spring-common/src/main/java/de/qaware/openapigeneratorforspring/common/schema/resolver/type/extension/spring/DefaultSpringResponseEntityTypeResolver.java
@@ -1,4 +1,4 @@
-package de.qaware.openapigeneratorforspring.test.app8;
+package de.qaware.openapigeneratorforspring.common.schema.resolver.type.extension.spring;
 
 import com.fasterxml.jackson.databind.JavaType;
 import de.qaware.openapigeneratorforspring.common.annotation.AnnotationsSupplier;
@@ -6,12 +6,13 @@ import de.qaware.openapigeneratorforspring.common.schema.Schema;
 import de.qaware.openapigeneratorforspring.common.schema.resolver.SchemaBuilderFromType;
 import de.qaware.openapigeneratorforspring.common.schema.resolver.type.TypeResolver;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 
 import java.util.function.Consumer;
 
-@Component
-public class ResponseEntityTypeResolver implements TypeResolver {
+public class DefaultSpringResponseEntityTypeResolver implements TypeResolver {
+
+    public static final int ORDER = DEFAULT_ORDER;
+
     @Override
     public boolean resolveFromType(JavaType javaType, AnnotationsSupplier annotationsSupplier, SchemaBuilderFromType schemaBuilderFromType, Consumer<Schema> schemaConsumer) {
         if (javaType.getRawClass().equals(ResponseEntity.class)) {
@@ -25,6 +26,6 @@ public class ResponseEntityTypeResolver implements TypeResolver {
 
     @Override
     public int getOrder() {
-        return DEFAULT_ORDER;
+        return ORDER;
     }
 }
