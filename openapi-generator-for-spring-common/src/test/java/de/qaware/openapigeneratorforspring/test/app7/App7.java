9bf4041d8e655eaebde7d9a8cf16c11b1abb2c1b
Dirk Kröhan <dirk.kroehan@qaware.de>
2020-09-08 14:03:34 +0200

    Add App7 test which uses a simple single controller.

---
+++ openapi-generator-for-spring-common/src/test/java/de/qaware/openapigeneratorforspring/test/app7/App7.java
@@ -0,0 +1,11 @@
+package de.qaware.openapigeneratorforspring.test.app7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App7 {
+    public static void main(String[] args) {
+        SpringApplication.run(App7.class, args);
+    }
+}
---
+++ openapi-generator-for-spring-common/src/test/java/de/qaware/openapigeneratorforspring/test/app7/App7SimpleController.java
@@ -0,0 +1,35 @@
+package de.qaware.openapigeneratorforspring.test.app7;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "simple")
+public class App7SimpleController {
+
+    @GetMapping("get-mapping")
+    public String getMappingReturnString() {
+        return null;
+    }
+
+    @PostMapping("post-mapping")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postMapping() {
+    }
+
+    @PutMapping("put-mapping")
+    public void putMapping() {
+    }
+
+    @DeleteMapping("delete-mapping")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMapping() {
+    }
+}
+
