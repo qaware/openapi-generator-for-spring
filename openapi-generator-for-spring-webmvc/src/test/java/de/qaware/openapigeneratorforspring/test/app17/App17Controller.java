839a2941d57e160491adc85af47849efbd195a5b
Andreas Grub <andreas.grub@qaware.de>
2020-10-30 08:36:15 +0100

    Enable everything again in App17 test, looks ok

--- openapi-generator-for-spring-webmvc/src/test/java/de/qaware/openapigeneratorforspring/test/app17/App17Controller.java
+++ openapi-generator-for-spring-webmvc/src/test/java/de/qaware/openapigeneratorforspring/test/app17/App17Controller.java
@@ -17,25 +17,25 @@ public class App17Controller {
         return null;
     }
 
-//    @GetMapping("/mapping2")
-//    public SomeDto mapping2(@RequestBody List<SomeDto> someDto) {
-//        return null;
-//    }
-//
-//    @GetMapping("/mapping3")
-//    public SomeDto mapping3() {
-//        return null;
-//    }
+    @GetMapping("/mapping2")
+    public SomeDto mapping2(@RequestBody List<SomeDto> someDto) {
+        return null;
+    }
+
+    @GetMapping("/mapping3")
+    public SomeDto mapping3() {
+        return null;
+    }
 
     @Value
     private static class SomeDto {
-        //        String stringProperty1;
+        String stringProperty1;
         @Nullable
         SomeDto someDtoProperty;
         List<SomeDto> listOfSomeDtos;
-        //        @Nullable
-//        List<SomeDto> nullableListOfSomeDtos;
-//        Set<List<SomeDto>> setOfListOfSomeDtos;
+        @Nullable
+        List<SomeDto> nullableListOfSomeDtos;
+        Set<List<SomeDto>> setOfListOfSomeDtos;
         @Nullable
         Set<List<SomeDto>> nullableSetOfListOfSomeDtos;
     }
