4b5002d87928e7489e606bb9f63709cfc9e61d6e
Christian Fritz <christian.fritz@qaware.de>
2023-01-24 18:18:44 +0100

    Fixes all reaming compile errors which occurred due to the dependency update.

--- openapi-generator-for-spring-test/src/test/java/de/qaware/openapigeneratorforspring/test/AbstractOpenApiGeneratorWebIntTest.java
+++ openapi-generator-for-spring-test/src/test/java/de/qaware/openapigeneratorforspring/test/AbstractOpenApiGeneratorWebIntTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
